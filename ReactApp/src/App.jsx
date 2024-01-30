3af2c096d4b64bd00b6f525f7def72a12c819f72
Karan01ingh <Singhkunal05046@gmail.com>
2024-01-30 16:38:17 +0530

    completed my first basic react progarme

--- ReactApp/src/App.jsx
+++ ReactApp/src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from 'react'
 
 function App() {
+  const addvalue=()=>{
+    setCounter(counter+1);
+  }
+  const minusvalue=()=>{
+    setCounter(counter-1);
+  }
+  let [counter, setCounter] = useState(15);
   return (
     <div>
       <h1>REACT WITH VITE</h1>
-      <h2>Counter value:5</h2>
+      <h2>Counter value:{counter}</h2>
+      <button onClick={addvalue} >Add value:{counter}</button>
+      <br />
+      <button onClick={minusvalue}>Decrease value:{counter}</button>
     </div>
   )
 }
